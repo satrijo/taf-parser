a5824cd26710cecebc4b202048a5c38cd11c7791
satriyo <stryuw@gmail.com>
2023-08-10 13:20:25 +0700

    back to process

--- utils/latest.js
+++ utils/latest.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const bearer =
-  "Bearer " + `c000561fd431399e706fc6d495606ebd655871a46807171c24c2e3704a46`;
-const url = `http://172.19.3.8/api/v1/`;
+const bearer = "Bearer " + `${process.env.TOKEN}`;
+const url = `${process.env.IP_SOURCE}/api/v1/`;
 
 const getTAF = async (icao, type) => {
   const { data } = await axios.get(`${url}${type}/${icao}`, {
